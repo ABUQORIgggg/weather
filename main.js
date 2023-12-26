bad8ff95fb8d16ea614f8aa3df0f3af0edaf1132
ABUQORIgggg <senseipubg2@gmail.com>
2023-12-26 19:44:45 +0500

    ASD

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let lat = document.querySelector('lat')
 let wnd = document.querySelector('#wind span')
 let desc = document.querySelector('#description')
 let wrapper = document.querySelector('.swiper-wrapper')
+let today = document.querySelector('#today')
 console.log(wrapper)
 const API_KEY = "06z87yaos4kclazy07jxupkbgulmnpkqmjv27k3r";
 const API_URL = "https://www.meteosource.com/api/v1/free/";
@@ -30,7 +31,7 @@ async function fetchWeather(place_id, lat, lon, sections) {
             // If place_id is available, use it
             url = `${API_URL}point?place_id=${place_id}&sections=${sections}&timezone=Asia%2FTashkent&language=en&units=auto&key=${API_KEY}`;
         } else if (lat && lon) {
-            // If lat and lon are available, use them
+            // If lat and lon are available, use them+
             url = `${API_URL}point?lat=${lat}&lon=${lon}&sections=${sections}&timezone=Asia%2FTashkent&language=en&units=auto&key=${API_KEY}`;
         } else {
             throw new Error("Either place_id or lat+lon must be specified.");
@@ -39,19 +40,69 @@ async function fetchWeather(place_id, lat, lon, sections) {
         const response = await fetch(url);
         const data = await response.json();
         console.log(data.hourly.data)
+
+        let dateis = data.hourly.data[0]
+        let currentTime = new Date()
+
         data.hourly.data.map(item => {
             let slide = document.createElement('div')
             let time = document.createElement('p')
             let gettime = item.date.slice(11, 16)
+            let local = item.date.slice(11, 13)
             let img = document.createElement('img')
             let temp = document.createElement('p')
 
+            console.log(currentTime.getHours() == local)
             if (item.weather === "overcast" || item.weather === "cloudy" || item.weather === 'partly_clear') { img.setAttribute('src', './images/cloudly.png') || img.setAttribute('src', './images/overcast.png') }
-            if (item.weather === "sun") { g.setAttribute('src', './images/sun.png') }
+            if (item.weather === "mostly_clear" && currentTime.getHours() == local) { img.setAttribute('src', './images/night.png')  }
+            if (item.weather === "sun" || item.weather === "partly_sunny") { img.setAttribute('src', './images/sunny.png') }
+            if (item.weather === "fog") { img.setAttribute('src', './images/wind.png') }
+            if (item.weather === "light_rain") { img.setAttribute('src', './images/rain-lighting.png') }
+            if (item.weather === "rain") { img.setAttribute('src', './images/rain.png') }
+            if (item.weather === "mostly_cloudy") { img.setAttribute('src', './images/overcast.png') }
 
             img.classList.add('max-w-[80px]')
             time.innerHTML = gettime
-            temp.innerHTML = item.temperature
+            temp.innerHTML = item.temperature + "°"
+            function WaitMonth() {
+                let t = item.date.slice(5, 7)
+                if (t == "12") {
+                    return "December"
+                } else if (t == "11") {
+                    return "November"
+                } else if (t == "10") {
+                    return "October"
+                } else if (t == "9") {
+                    return "September"
+                } else if (t == "8") {
+                    return "August"
+                } else if (t == "7") {
+                    return "July"
+                } else if (t == "6") {
+                    return "Juny"
+                } else if (t == "5") {
+                    return "May"
+                } else if (t == "4") {
+                    return "April"
+                } else if (t == "3") {
+                    return "March"
+                } else if (t == "2") {
+                    return "February"
+                } else {
+                    return "January"
+                }
+            }
+            function WaitingDay() {
+                let t = dateis.date.slice(8, 10)
+
+                if (t.includes("T")) {
+                    return t = t.slice(0, t.indexOf("T"));
+                }
+
+                return t
+            }
+            today.innerHTML = WaitMonth() + ", " + WaitingDay()
+            
 
             slide.classList.add('swiper-slide', 'w-full', 'flex', 'flex-col', 'items-center', 'py-5')
 
---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "globDirectory": ".",
+    "globPatterns": [
+        "**/*.{html,css,js,json,png,jpg,jpeg,svg}"
+    ],
+    "swDest": "./service-worker.js"
+};
